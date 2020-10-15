e9f9684a4ccddd3a7ae9da1dc9e8e40ed58929af
JotaMSC <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-15 08:32:13 +0200

    Actualizar

---
+++ Ejercicio4.java
@@ -0,0 +1,27 @@
+/**
+*Muestra programa que muestre horario de clase.
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio4 { // Clase principal
+	public static void main(String[] args) {
+		System.out.println("┌───────┬────────┬───────────┬────────┬────────┐");
+		System.out.println("│ Lunes │ Martes │ Miercoles │ Jueves │ Viernes|");
+		System.out.println("├───────┼────────┼───────────┼────────┼────────┤");
+		System.out.println("│ Prog  │ EED    │   Prog    │   EED  │  Prog  |");
+		System.out.println("├───────┼────────┼───────────┼────────┼────────┤");
+		System.out.println("│ Prog  │ EED    │   Prog    │   EED  │  Prog  |");
+		System.out.println("├───────┼────────┼───────────┼────────┼────────┤");
+		System.out.println("│ Prog  │ EED    │   Prog    │  BBDD  │   Sis  |");
+		System.out.println("├───────┼────────┼───────────┼────────┼────────┤");
+		System.out.println("│ BBDD  │ LenMar │   Prog    │  BBDD  │   Sis  |");
+		System.out.println("├───────┼────────┼───────────┼────────┼────────┤");
+		System.out.println("│ BBDD  │ LenMar │   Prog    │  BBDD  │   Sis  |");
+		System.out.println("├───────┼────────┼───────────┼────────┼────────┤");
+		System.out.println("│ BBDD  │ LenMar │   Prog    │  FOL   │   Sis  |");
+		System.out.println("└───────┴────────┴───────────┴────────┴────────┘");
+		
+	}
+}
+
+
---
+++ Nombredirecciontelefono.java
@@ -0,0 +1,12 @@
+/**
+*Muestra por pantalla mi nombre "Juan Miguel"
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Nombredirecciontelefono { // Clase principal
+	public static void main(String[] args) {
+		System.out.println("Nombre: Juan Miguel Sánchez Cruzado");
+		System.out.println("Dirección: Calle Sasa,n99");
+		System.out.println("Teléfono: 454885877");
+	}
+}
