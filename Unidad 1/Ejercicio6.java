13d2b2aaf34c22a4538d0b3086be69ebce16ebb3
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-19 14:57:33 +0200

    actualizacion

---
+++ Unidad 1/Ejercicio6.java
@@ -0,0 +1,22 @@
+/**
+*Escribe un programa que pinte por pantalla una pirámide rellena a base de
+asteriscos. La base de la pirámide debe estar formada por 9 asteriscos.
+Ejemplo:
+*
+***
+*****
+*******
+*********
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio6 { // Clase principal
+	public static void main(String[] args) {
+		System.out.printf("%-4s %s \n","","*");
+		System.out.printf("%-3s %s \n","","***");
+		System.out.printf("%-2s %s \n","","*****");
+		System.out.printf("%-1s %s \n","","*******");
+		System.out.printf("%s %s \n","","*********");
+
+	}
+}
