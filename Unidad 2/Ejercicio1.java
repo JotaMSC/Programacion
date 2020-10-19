06b51a120ff3f32d2434387730b51e66afba2a16
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-19 16:04:40 +0200

    añadimos unidad 2

---
+++ Unidad 2/Ejercicio1.java
@@ -0,0 +1,13 @@
+/**
+Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+el valor de cada variable, la suma, la resta, la división y la multiplicación.
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio1 { // Clase principal
+	public static void main(String[] args) {
+		System.out.printf("%s %s \n","Hola"," mundo");
+
+	}
+}
