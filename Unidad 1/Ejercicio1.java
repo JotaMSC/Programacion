1c21eddcf0e8d4e8689f0e4ef6f082aec33cc626
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-19 09:28:47 +0200

    actualizacion

---
+++ Unidad 1/Ejercicio1.java
@@ -0,0 +1,10 @@
+/**
+*Escribe una programa que muestre tu nombre por pantalla.
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio1 { // Clase principal
+	public static void main(String[] args) {
+		System.out.println("Juan Miguel Sanchez Cruzado");
+	}
+}
