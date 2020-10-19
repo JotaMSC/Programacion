ebecb952be6121e2e9e883cd12c41f389c3cc925
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-19 09:54:40 +0200

    actualizacion

---
+++ Unidad 1/Ejercicio2.java
@@ -0,0 +1,14 @@
+/**
+*Modifica el programa anterior para que además se muestre tu dirección y
+tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+separadas.
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio2 { // Clase principal
+	public static void main(String[] args) {
+		System.out.println("Juan Miguel Sanchez Cruzado");
+		System.out.println("Calle Sol");
+		System.out.println("888999777");
+	}
+}
---
+++ Unidad 1/Ejercicio3.java
@@ -0,0 +1,21 @@
+/**
+*Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su
+correspondiente traducción al castellano. Las palabras deben estar distribuidas
+en dos columnas y alineadas a la izquierda.
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio3 { // Clase principal
+	public static void main(String[] args) {
+		System.out.printf("%-10s %-10s\n", "computer", "ordenador");
+		System.out.printf("%-10s %-10s\n", "student", "estudiante");
+		System.out.printf("%-10s %-10s\n", "cat", "gato");
+		System.out.printf("%-10s %-10s\n", "penguin", "pingüino");
+		System.out.printf("%-10s %-10s\n", "machine", "máquina");
+		System.out.printf("%-10s %-10s\n", "nature", "naturaleza");
+		System.out.printf("%-10s %-10s\n", "light", "luz");
+		System.out.printf("%-10s %-10s\n", "green", "verde");
+		System.out.printf("%-10s %-10s\n", "book", "libro");
+		System.out.printf("%-10s %-10s\n", "pyramid", "pirámide");
+	}
+}
