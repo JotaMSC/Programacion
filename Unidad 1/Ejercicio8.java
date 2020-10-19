febd4546221bfee6753794375d60ea5dd7c23971
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-19 15:59:04 +0200

    findeunidad1

---
+++ Unidad 1/Ejercicio8.java
@@ -0,0 +1,22 @@
+/**
+Igual que el programa anterior, pero esta vez la pirámide debe aparecer
+invertida, con el vértice hacia abajo.
+Ejemplo:
+*********
+ *     *
+  *   *
+   * *
+    *
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio8 { // Clase principal
+	public static void main(String[] args) {
+		System.out.printf("%s %-9s \n","","*********");
+		System.out.printf("%-1s %s %-3s %s \n","","*","","*");
+		System.out.printf("%-2s %s %-1s %s \n","","*","","*");
+		System.out.printf("%-3s %s %s \n","","*","*");
+		System.out.printf("%-4s %s \n","","*");
+
+	}
+}
