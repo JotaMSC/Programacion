a446b496277d03f06d7174baef68ceb8ed7e2dcf
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-20 23:31:25 +0200

    Ejercicio 2 resuelto

---
+++ Unidad 2/Ejercicio2.java
@@ -0,0 +1,15 @@
+/**
+Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+pantalla de tal forma que el resultado del programa sea el mismo que en el
+ejercicio 1 del capítulo 1.
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio2 { // Clase principal
+	public static void main(String[] args) {
+		String miNombre = "Juan Miguel";
+		System.out.println(miNombre);
+		
+
+	}
+}
