ec07157c56de0af306e7c3b769791aaef2388984
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-20 23:36:38 +0200

    Ejercicio3 hecho

---
+++ Unidad 2/Ejercicio3.java
@@ -0,0 +1,16 @@
+/**
+Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. Muestra los valores de esas variables por pantalla de tal forma que el
+resultado del programa sea el mismo que en el ejercicio 2.
+*
+*@author Juan Miguel Sánchez Cruzado
+*/
+public class Ejercicio3 { // Clase principal
+	public static void main(String[] args) {
+		String miNombre = "Juan Miguel";
+		String miDireccion = " Calle Alba";
+		String miTelefono = "949944000";
+		System.out.println((miNombre) + (miDireccion) + (miTelefono));
+		
+
+	}
+}
