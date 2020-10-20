4349cd7cb47d9ad0a2cff10d7178dc604baf62b1
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-20 23:27:00 +0200

    Ejercicio1 resuelto

--- Unidad 2/Ejercicio1.java
+++ Unidad 2/Ejercicio1.java
@@ -7,7 +7,15 @@ el valor de cada variable, la suma, la resta, la división y la multiplicación.
 */
 public class Ejercicio1 { // Clase principal
 	public static void main(String[] args) {
-		System.out.printf("%s %s \n","Hola"," mundo");
+		int x = 144;
+		int y = 999;
+		System.out.println("El valor de x es:" + x);
+		System.out.println("El valor de y es:" + y);
+		System.out.println("La suma de ambos es:" + (x+y));
+		System.out.println("La resta de ambos es:" + (x-y));
+		System.out.println("La división de x/y seria:" + (x/y));
+		System.out.println("Y la multiplicacion:" + (x*y));
+		
 
 	}
 }
