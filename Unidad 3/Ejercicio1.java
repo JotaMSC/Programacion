7096796bb92e86c2a1ff7ebee8ca6fc70d5cfa55
JuanMiguel <juanmiguel.sanchez.alu@iescampanillas.com>
2020-10-25 19:07:11 +0100

    realizacion actividad 1

--- Unidad 3/Ejercicio1.java
+++ Unidad 3/Ejercicio1.java
@@ -1,10 +1,22 @@
 /**
-Comienzo de Unidad 3
+Realiza un programa que pida dos números y que luego muestre el resultado
+de su multiplicación.
 *
 *@author Juan Miguel Sánchez Cruzado
 */
 public class Ejercicio1 { // Clase principal
 	public static void main(String[] args) {
-
+		int x;
+		int y;
+		String dato;
+		
+		System.out.print("Introduce el primer numero de la multiplicacion: ");
+		dato = System.console().readLine();
+		x = Integer.parseInt( dato );
+		System.out.print("El segundo numero: ");
+		dato = System.console().readLine();
+		y = Integer.parseInt( dato );
+		
+		System.out.println(x + "*" + y + "=" + (x * y));
 	}
 }
